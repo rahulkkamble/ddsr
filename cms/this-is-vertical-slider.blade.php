c5dc9314f4d3b1e7ccc9e7b7484159728f0c6fde
Rahul <faizalimulla@gmail.com>
2024-07-11 20:37:17 +0530

    banner added

--- cms/this-is-vertical-slider.blade.php
+++ cms/this-is-vertical-slider.blade.php
@@ -2,48 +2,51 @@
     <div class="sectionBg -left-2 w-1/2 lg:w-1/1 z-2">
         <div class="verticalSlider-images__images">
 
-            <div class="is-active">
-                <img src="/frontend/ddsr/assets/img/COMPLETED/DDSR_Anuparva.jpg" alt="image" class="img-ratio">
-
-                <a href="#"
-                   class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
-                    <span class="text-15 fw-500 text-white">Explore</span>
-                </a>
-
-                <div class="qr-on-top">
-                    <img src="https://ddsrgroup.com/storage/laravel-grapesjs/media/sample_qr_1.jpg" alt="QR">
-                </div>
-            </div>
-
-            <div class="">
-                <img src="/frontend/ddsr/assets/img/COMPLETED/DDSR_Vista.jpg" alt="image" class="img-ratio">
-
-                <a href="#"
-                   class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
-                    <span class="text-15 fw-500 text-white">Explore</span>
-                </a>
-                <div class="qr-on-top">
-                    <img src="https://ddsrgroup.com/storage/laravel-grapesjs/media/sample_qr_1.jpg" alt="QR">
-                </div>
-            </div>
-
             <div class="">
                 <img src="/frontend/ddsr/assets/img/COMPLETED/Ekta_Vihar.jpg" alt="image" class="img-ratio">
 
                 <a href="#"
-                   class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
+                    class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
                     <span class="text-15 fw-500 text-white">Explore</span>
                 </a>
                 <div class="qr-on-top">
                     <img src="https://ddsrgroup.com/storage/laravel-grapesjs/media/sample_qr_1.jpg" alt="QR">
                 </div>
             </div>
+
+            <div class="">
+                <img src="https://ddsrgroup.com/storage/laravel-grapesjs/media/ANUVISHWA_CHS.jpg" alt="image"
+                    class="img-ratio">
+
+                <a href="#"
+                    class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
+                    <span class="text-15 fw-500 text-white">Explore</span>
+                </a>
+                <div class="qr-on-top">
+                    <img src="https://ddsrgroup.com/storage/laravel-grapesjs/media/sample_qr_1.jpg" alt="QR">
+                </div>
+            </div>
+
+            <div class="is-active">
+                <img src="/frontend/ddsr/assets/img/COMPLETED/DDSR_Anuparva.jpg" alt="image" class="img-ratio">
+
+                <a href="#"
+                    class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
+                    <span class="text-15 fw-500 text-white">Explore</span>
+                </a>
+
+                <div class="qr-on-top">
+                    <img src="https://ddsrgroup.com/storage/laravel-grapesjs/media/sample_qr_1.jpg" alt="QR">
+                </div>
+            </div>
+
+
 
             <div class="">
                 <img src="/frontend/ddsr/assets/img/COMPLETED/JAY_SHRI_KRUSHNA.jpg" alt="image" class="img-ratio">
 
                 <a href="#"
-                   class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
+                    class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
                     <span class="text-15 fw-500 text-white">Explore</span>
                 </a>
                 <div class="qr-on-top">
@@ -52,10 +55,11 @@
             </div>
 
             <div class="">
-                <img src="/frontend/ddsr/assets/img/COMPLETED//KALAKAR_PATRAKAR_CHS.jpg" alt="image" class="img-ratio">
+                <img src="/frontend/ddsr/assets/img/COMPLETED//KALAKAR_PATRAKAR_CHS.jpg" alt="image"
+                    class="img-ratio">
 
                 <a href="#"
-                   class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
+                    class="verticalSlider-images__button flex-center size-130 rounded-full bg-accent-1-50 blur-1 border-white-10">
                     <span class="text-15 fw-500 text-white">Explore</span>
                 </a>
                 <div class="qr-on-top">
@@ -71,46 +75,8 @@
             <div class="col-xl-3 col-lg-4 offset-lg-7">
                 <div class="verticalSlider__wrap">
                     <div class="verticalSlider js-verticalSlider" data-gap="130" data-vertical
-                         data-slider-cols="xl-3 lg-3 md-1 sm-1 base-1" data-pagination="js-verticalSlider-pagination">
+                        data-slider-cols="xl-3 lg-3 md-1 sm-1 base-1" data-pagination="js-verticalSlider-pagination">
                         <div class="swiper-wrapper flex-column h-auto">
-
-                            <div class="swiper-slide d-flex items-center">
-                                <div class="d-flex x-gap-30 y-gap-30">
-                                    <div class="text-40 lg:text-30 sm:text-21 text-white mt-20 round-border-icon">
-                                        <div class="dot"></div>
-                                    </div>
-                                    <div class="verticalSlider-content">
-                                        <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
-                                            Anuparva (2016)
-                                        </h4>
-                                        <p class="text-17 text-white mt-20">
-                                            Location
-                                        </p>
-                                        <p class="text-17 text-white mt-20">
-                                            MAHARERA NO : ---
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="swiper-slide d-flex items-center">
-                                <div class="d-flex x-gap-30 y-gap-30">
-                                    <div class="text-40 lg:text-30 sm:text-21 text-white mt-20 round-border-icon">
-                                        <div class="dot"></div>
-                                    </div>
-                                    <div class="verticalSlider-content">
-                                        <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
-                                            Vista (2017)
-                                        </h4>
-                                        <p class="text-17 text-white mt-20">
-                                            Location
-                                        </p>
-                                        <p class="text-17 text-white mt-20">
-                                            MAHARERA NO : ---
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
 
                             <div class="swiper-slide d-flex items-center">
                                 <div class="d-flex x-gap-30 y-gap-30">
@@ -120,10 +86,10 @@
                                     <div>
                                         <div class="verticalSlider-content">
                                             <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
-                                                Vihar (2018)
+                                                Vihar (2023)
                                             </h4>
                                             <p class="text-17 text-white mt-20">
-                                                Location
+                                                Kalamboli, Navi Mumbai
                                             </p>
                                             <p class="text-17 text-white mt-20">
                                                 MAHARERA NO : ---
@@ -140,10 +106,10 @@
                                     </div>
                                     <div class="verticalSlider-content">
                                         <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
-                                            Jai Shri Krishna (2019)
+                                            Anuvishwa (2024)
                                         </h4>
                                         <p class="text-17 text-white mt-20">
-                                            Location
+                                            New Panvel
                                         </p>
                                         <p class="text-17 text-white mt-20">
                                             MAHARERA NO : ---
@@ -159,10 +125,50 @@
                                     </div>
                                     <div class="verticalSlider-content">
                                         <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
-                                            Kalakar Patrakar (2020)
+                                            Anuparva (2025)
                                         </h4>
                                         <p class="text-17 text-white mt-20">
-                                            Location
+                                            Khanda Colony, Panvel
+                                        </p>
+                                        <p class="text-17 text-white mt-20">
+                                            MAHARERA NO : ---
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div class="swiper-slide d-flex items-center">
+                                <div class="d-flex x-gap-30 y-gap-30">
+                                    <div class="text-40 lg:text-30 sm:text-21 text-white mt-20 round-border-icon">
+                                        <div class="dot"></div>
+                                    </div>
+                                    <div class="verticalSlider-content">
+                                        <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
+                                            Jai Shri Krishna (2026)
+                                        </h4>
+                                        <p class="text-17 text-white mt-20">
+                                            Kalamboli, Navi Mumbai
+                                        </p>
+                                        <p class="text-17 text-white mt-20">
+                                            MAHARERA NO : ---
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="swiper-slide d-flex items-center">
+                                <div class="d-flex x-gap-30 y-gap-30">
+                                    <div class="text-40 lg:text-30 sm:text-21 text-white mt-20 round-border-icon">
+                                        <div class="dot"></div>
+                                    </div>
+                                    <div class="verticalSlider-content">
+                                        <h4 class="text-40 lg:text-30 sm:text-21 text-white mt-20">
+                                            Kalakar Patrakar (2026)
+                                        </h4>
+                                        <p class="text-17 text-white mt-20">
+                                            Khanda Colony, New Panvel
                                         </p>
                                         <p class="text-17 text-white mt-20">
                                             MAHARERA NO : ---
